65f54d3e958fe4f2d392a4856f9c84aa7d59a150
Jonathan Forrider <jd4rider@gmail.com>
2024-03-04 10:51:33 -0700

    added language picker, needs wired up

--- templates/bible.go
+++ templates/bible.go
@@ -15,9 +15,84 @@ type BibleId struct {
 	Name string
 }
 
+type Lang struct {
+	id              string
+	name            string
+	nameLocal       string
+	script          string
+	scriptDirection string
+}
+
 type BookId = BibleId
 
 type ChapId = BibleId
+
+func removeDuplicate[T comparable](sliceList []T) []T {
+	allKeys := make(map[T]bool)
+	list := []T{}
+	for _, item := range sliceList {
+		if _, value := allKeys[item]; !value {
+			allKeys[item] = true
+			list = append(list, item)
+		}
+	}
+	return list
+}
+
+func Languages() []Lang {
+	url := "https://api.scripture.api.bible/v1/bibles"
+
+	bibleClient := http.Client{
+		Timeout: time.Second * 20,
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+		return []Lang{}
+	}
+
+	req.Header.Set("api-key", os.Getenv("API_KEY"))
+
+	res, getErr := bibleClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+		return []Lang{}
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := io.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+		return []Lang{}
+	}
+
+	var body1 map[string]interface{}
+	jsonErr := json.Unmarshal(body, &body1)
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+		return []Lang{}
+	}
+	bibleData := body1["data"].([]interface{}) //[0].(map[string]interface{})
+
+	langIds := []Lang{}
+	for i := 0; i < len(bibleData); i++ {
+		//if bibleData[i].(map[string]interface{})["language"].(map[string]interface{})["name"].(string) == "English" {
+		//fmt.Println(bibleData[i].(map[string]interface{})["name"].(string))
+		//}
+		langIds = append(langIds, Lang{
+			id:        bibleData[i].(map[string]interface{})["language"].(map[string]interface{})["id"].(string),
+			name:      bibleData[i].(map[string]interface{})["language"].(map[string]interface{})["name"].(string),
+			nameLocal: bibleData[i].(map[string]interface{})["language"].(map[string]interface{})["nameLocal"].(string),
+		})
+	}
+	//fmt.Println(bibleData["name"].(string))
+
+	return removeDuplicate(langIds)
+}
 
 func Bibleid() []BibleId {
 	url := "https://api.scripture.api.bible/v1/bibles"
