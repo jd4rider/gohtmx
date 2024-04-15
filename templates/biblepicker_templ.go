dac75c52f27d44163e8900371b01e0d45d7b01e4
Jonathan Forrider <jd4rider@gmail.com>
2024-04-15 12:18:41 -0600

    Made the picker section sticky

--- templates/biblepicker_templ.go
+++ templates/biblepicker_templ.go
@@ -54,7 +54,7 @@ func Biblepicker(langid string) templ.Component {
 				return templ_7745c5c3_Err
 			}
 		}
-		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</select><div class=\"pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700\"><svg class=\"fill-current h-4 w-4\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\"><path d=\"M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z\"></path></svg></div></div></div><button id=\"biblepickerclick\" type=\"submit\" class=\"invisible\" type=\"button\" hidden>Submit</button></form><script>\n\t\t\tfunction submitandclearbible() {\n\t\t\t\tdocument.getElementById('biblepickerclick').click(); \n\t\t\t\tdocument.getElementById('biblecontent').innerHTML = '';\n\t\t\t\tdocument.getElementById('chapterpicker').innerHTML = '';\n\t\t\t}\t\n\t\t</script></div>")
+		_, templ_7745c5c3_Err = templ_7745c5c3_Buffer.WriteString("</select><div class=\"pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700\"><svg class=\"fill-current h-4 w-4\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 20 20\"><path d=\"M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z\"></path></svg></div></div></div><button id=\"biblepickerclick\" type=\"submit\" class=\"invisible\" type=\"button\" hidden>Submit</button></form><script>\n\t\t\tfunction submitandclearbible() {\n\t\t\t\tdocument.getElementById('biblepickerclick').click(); \n\t\t\t\tdocument.getElementById('biblecontent').innerHTML = '';\n\t\t\t\tdocument.getElementById('chapterpicker').innerHTML = '';\n\t\t\t\twindow.scrollTo(0, 0);\n\t\t\t}\t\n\t\t</script></div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
